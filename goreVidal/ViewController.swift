4b518e7faafcbede825c995b042960d6d74d41f4
James Todd <james.c.todd@gmail.com>
2017-10-17 05:36:56 -0400

    created a conditional statement to allow the coder to either add or subtract textfields

--- goreVidal/ViewController.swift
+++ goreVidal/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var coolLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
@@ -18,6 +18,14 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         coolLabel.text = "Answer is... \(Double(text1.text!)! +  Double(text2.text!)!)"
+        
+        var addition = true
+        
+        if addition {
+            coolLabel.text = "Answer is... \(Double(text1.text!)! +  Double(text2.text!)!)"
+        } else {
+            coolLabel.text = "Answer is... \(Double(text1.text!)! -  Double(text2.text!)!)"
+        }
     }
     
     
@@ -25,12 +33,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
